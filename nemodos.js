2a70641670dacc0a52eef95f703b91e7fc7caa6b
Barrington Lloyd-Lovett <barrington@stripe.com>
2016-10-25 20:16:51 -0700

    the first javascript additions

---
+++ nemodos.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  console.log("i'm drunk and you're insane...");
+  $('#release').click(function() {
+    $('#site_greeting').hide();
+    $('#release').hide();
+  });
+});
